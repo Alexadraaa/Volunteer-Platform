a21b7999ee5ed8520c7136807561092338b4369a
AlexisGiannoutsos <101717093+AlexisGiannoutsos@users.noreply.github.com>
2024-01-22 12:38:25 +0200

    Update admin.php

--- code/html/admin.php
+++ code/html/admin.php
@@ -45,47 +45,43 @@
     <div id="map"></div>
     
     <!-- Toggle Markers Button -->
-    
-    <!-- Add these modifications to your existing HTML for each button -->
+   
+
+
 <button id="toggleMarkersBtn1">
-    <img class="icon-next-to-button" src="home.png" alt="Base Icon">
-    Toggle Base
-</button>
-
-<button id="toggleMarkersBtn2">
-    <img class="icon-next-to-button" src="yellowcar.png" alt="NonActiveTaskCar Icon">
-    Toggle nonActiveTaskCar
-</button>
-
-<button id="toggleMarkersBtn3">
     <img class="icon-next-to-button" src="bluecar.png" alt="ActiveTaskCar Icon">
     Toggle ActiveTaskCar
 </button>
 
-<button id="toggleMarkersBtn4">
-    <img class="icon-next-to-button" src="greendonate.png" alt="TakenDonation Icon">
-    Toggle TakenDonation
+<button id="toggleMarkersBtn2">
+    <img class="icon-next-to-button" src="yellowcar.png" alt="InactiveTaskCar Icon">
+    Toggle InactiveTaskCar
 </button>
 
+<button id="toggleMarkersBtn3">
+    <img class="icon-next-to-button" src="greendonate.png" alt="ActiveDonation Icon">
+    Toggle ActiveDonation
+</button>
+
+<button id="toggleMarkersBtn4">
+    <img class="icon-next-to-button" src="orangedonate.png" alt="InactiveDonation Icon">
+    Toggle InactiveDonation
+
 <button id="toggleMarkersBtn5">
-    <img class="icon-next-to-button" src="orangedonate.png" alt="NonTakenDonation Icon">
-    Toggle nonTakenDonation
+    <img class="icon-next-to-button" src="greenrequest.png" alt="ActiveRequest Icon">
+    Toggle ActiveRequest
 </button>
 
 <button id="toggleMarkersBtn6">
-    <img class="icon-next-to-button" src="greenexclamation-mark.png" alt="TakenRequest Icon">
-    Toggle TakenRequest
+    <img class="icon-next-to-button" src="orangerequest.png" alt="InactiveRequest Icon">
+    Toggle InactiveRequest
 </button>
-
-<button id="toggleMarkersBtn7">
-    <img class="icon-next-to-button" src="orangeexclamation-mark.png" alt="NonTakenRequest Icon">
-    Toggle nonTakenRequest
-</button>
-
-
 
     <!-- Leaflet JavaScript -->
     <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+
+    <!-- jQuery (for AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 
     <script>
         // Function to toggle the menu visibility
@@ -125,204 +121,204 @@
              window.location.href = 'initialpage.html';
         }
 
-        // Leaflet Map Initialization
-        var map = L.map('map').setView([38.2488, 21.7345], 16); // Set the coordinates for Georgiou Square in Patras
+     // Leaflet Map Initialization
+     var map = L.map('map').setView([38.2488, 21.7345], 16); // Set the coordinates for Georgiou Square in Patras
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '© OpenStreetMap contributors'
-        }).addTo(map);
-        
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
 
-        // Marker Example
-        L.marker([38.2488, 21.7345]).addTo(map)
-            .bindPopup('Georgiou Square, Patras, Greece.');
+// Custom Icon for Markers
+var baseIcon = L.icon({
+    iconUrl: '../html/home.png',
+    iconSize: [30, 30],
+    iconAnchor: [20, 15],
+    popupAnchor: [-5, -10]
+});
 
-      // Custom Icon for Markers
-    var base = L.icon({
-        iconUrl: '../html/home.png',
-        iconSize: [30, 30], // Adjust the size of your icon
-        iconAnchor: [20, 15], // Adjust the anchor point of your icon
-        popupAnchor: [-5, -10] // Adjust the popup anchor of your icon
+// Custom Icon for Markers
+var activeTaskCarIcon = L.icon({
+    iconUrl: '../html/bluecar.png',
+    iconSize: [30, 30],
+    iconAnchor: [20, 15],
+    popupAnchor: [-5, -10]
+});
+
+// Custom Icon for Markers
+var inactiveTaskCarIcon = L.icon({
+    iconUrl: '../html/yellowcar.png',
+    iconSize: [30, 30],
+    iconAnchor: [20, 15],
+    popupAnchor: [-5, -10]
+});
+
+// Custom Icon for Markers
+var activeDonationIcon = L.icon({
+    iconUrl: '../html/greendonate.png',
+    iconSize: [30, 30],
+    iconAnchor: [20, 15],
+    popupAnchor: [-5, -10]
+});
+
+// Custom Icon for Markers
+var inactiveDonationIcon = L.icon({
+    iconUrl: '../html/orangedonate.png',
+    iconSize: [30, 30],
+    iconAnchor: [20, 15],
+    popupAnchor: [-5, -10]
+});
+
+// Custom Icon for Markers
+var activeRequestIcon = L.icon({
+    iconUrl: '../html/greenrequest.png',
+    iconSize: [30, 30],
+    iconAnchor: [20, 15],
+    popupAnchor: [-5, -10]
+});
+
+// Custom Icon for Markers
+var inactiveRequestIcon = L.icon({
+    iconUrl: '../html/orangerequest.png',
+    iconSize: [30, 30],
+    iconAnchor: [20, 15],
+    popupAnchor: [-5, -10]
+});
+
+
+
+// Function to fetch marker data from PHP script
+function fetchMarkers() {
+    $.ajax({
+        url: 'get_markers.php',
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            // Add markers to the map dynamically
+            addMarkers(data);
+        },
+        error: function(error) {
+            console.error('Error fetching markers:', error);
+        }
+    });
+}
+
+// Function to add markers dynamically to the map
+function addMarkers(markerData) {
+    // Loop through the marker data and add markers to the map
+    markerData.forEach(function(marker) {
+        var customIcon = getCustomIcon(marker.type,marker.activity);
+
+        var newMarker = L.marker([marker.lat, marker.lng], { icon: customIcon }).bindPopup(marker.type);
+        getMarkersGroup(marker.type).addLayer(newMarker);
     });
 
-       // Custom Icon for Markers
-    var ActiveTaskCar = L.icon({
-        iconUrl: '../html/bluecar.png',
-        iconSize: [30, 30], // Adjust the size of your icon
-        iconAnchor: [20, 15], // Adjust the anchor point of your icon
-        popupAnchor: [-5, -10] // Adjust the popup anchor of your icon
-    });
+    // Add the marker groups to the map
+    addMarkerGroupsToMap();
+}
 
-    // Custom Icon for Markers
-    var nonActiveTaskCar = L.icon({
-        iconUrl: '../html/yellowcar.png',
-        iconSize: [30, 30], // Adjust the size of your icon
-        iconAnchor: [20, 15], // Adjust the anchor point of your icon
-        popupAnchor: [-5, -10] // Adjust the popup anchor of your icon
-    });
+// Function to get the appropriate custom icon based on marker type
+function getCustomIcon(type,activity) {
+    switch (type) {
+        case 'vehicle':
+            return activity === 'active' ? activeTaskCarIcon : inactiveTaskCarIcon;
+        case 'offer':
+            return activity === 'active' ? activeDonationIcon : inactiveDonationIcon;
+        case 'request':
+            return activity === 'active' ? activeRequestIcon : inactiveRequestIcon;
+        default:
+            return baseIcon; // Return a default icon if the type is not recognized
+    }
+}
 
-       // Custom Icon for Markers
-    var TakenDonation = L.icon({
-        iconUrl: '../html/greendonate.png',
-        iconSize: [30, 30], // Adjust the size of your icon
-        iconAnchor: [20, 15], // Adjust the anchor point of your icon
-        popupAnchor: [-5, -10] // Adjust the popup anchor of your icon
-    });
+// Function to get the corresponding marker group based on marker type
+function getMarkersGroup(type,activity) {
+    switch (type) {
+        case 'vehicle':
+            return activity === 'active' ? markersGroup1Active : markersGroup1Inactive;
+        case 'offer':
+            return activity === 'active' ? markersGroup2Active : markersGroup2Inactive;
+        case 'request':
+            return activity === 'active' ? markersGroup3Active : markersGroup3Inactive;
+      
+        // Add more cases as needed
+    }
+}
+
+var markersGroup1Active = L.layerGroup();
+var markersGroup1Inactive = L.layerGroup();
+var markersGroup2Active = L.layerGroup();
+var markersGroup2Inactive = L.layerGroup();
+var markersGroup3Active = L.layerGroup();
+var markersGroup3Inactive = L.layerGroup();
+var markersGroup4Active = L.layerGroup();
+var markersGroup4Inactive = L.layerGroup();
+
+// Function to add marker groups to the map
+function addMarkerGroupsToMap() {
+    if (!map.hasLayer(markersGroup1Active)) {
+        map.addLayer(markersGroup1Active);
+    }
+    if (!map.hasLayer(markersGroup1Inactive)) {
+        map.addLayer(markersGroup1Inactive);
+    }
+    if (!map.hasLayer(markersGroup2Active)) {
+        map.addLayer(markersGroup2Active);
+    }
+    if (!map.hasLayer(markersGroup2Inactive)) {
+        map.addLayer(markersGroup2Inactive);
+    }
+    if (!map.hasLayer(markersGroup3Active)) {
+        map.addLayer(markersGroup3Active);
+    }
+    if (!map.hasLayer(markersGroup3Inactive)) {
+        map.addLayer(markersGroup3Inactive);
+    }    
+   
     
-    // Custom Icon for Markers
-    var nonTakenDonation = L.icon({
-        iconUrl: '../html/orangedonate.png',
-        iconSize: [30, 30], // Adjust the size of your icon
-        iconAnchor: [20, 15], // Adjust the anchor point of your icon
-        popupAnchor: [-5, -10] // Adjust the popup anchor of your icon
-    });
+}
 
-       // Custom Icon for Markers
-    var TakenRequest = L.icon({
-        iconUrl: '../html/greenexclamation-mark.png',
-        iconSize: [30, 30], // Adjust the size of your icon
-        iconAnchor: [20, 15], // Adjust the anchor point of your icon
-        popupAnchor: [-5, -10] // Adjust the popup anchor of your icon
-    });
+// Function to toggle markers of a specific type
+function toggleMarkersByType(markersGroup) {
+    if (map.hasLayer(markersGroup)) {
+        map.removeLayer(markersGroup);
+    } else {
+        map.addLayer(markersGroup);
+    }
+}
 
-    var nonTakenRequest = L.icon({
-        iconUrl: '../html/orangeexclamation-mark.png',
-        iconSize: [30, 30], // Adjust the size of your icon
-        iconAnchor: [20, 15], // Adjust the anchor point of your icon
-        popupAnchor: [-5, -10] // Adjust the popup anchor of your icon
-    });
-  
-    var markersGroup1 = L.layerGroup();
-    var markersGroup2 = L.layerGroup();
-    var markersGroup3 = L.layerGroup();
-    var markersGroup4 = L.layerGroup();
-    var markersGroup5 = L.layerGroup();
-    var markersGroup6 = L.layerGroup();
-    var markersGroup7 = L.layerGroup();
+// Event listener for button click to toggle markers
+var toggleMarkersButton1 = document.getElementById('toggleMarkersBtn1');
+toggleMarkersButton1.addEventListener('click', function() {
+    toggleMarkersByType(markersGroup1Active);
+});
 
-        // Add markers of each type
-        var marker1  = L.marker([38.245823, 21.735651], { icon: base }).bindPopup('base');
-        var marker2  = L.marker([38.246644, 21.734562], { icon: base }).bindPopup('base');
-        
-        var marker3  = L.marker([38.244517, 21.732309], { icon: nonActiveTaskCar }).bindPopup('NonActiveCar');
-        var marker4  = L.marker([38.243287, 21.733747], { icon: nonActiveTaskCar }).bindPopup('NonActiveCar');
-        
-        var marker5  = L.marker([38.246859, 21.732566], { icon: ActiveTaskCar }).bindPopup('ActiveCar');
-        var marker6  = L.marker([38.248679, 21.737266], { icon: ActiveTaskCar }).bindPopup('ActiveCar');
+var toggleMarkersButton2 = document.getElementById('toggleMarkersBtn2');
+toggleMarkersButton2.addEventListener('click', function() {
+    toggleMarkersByType(markersGroup1Inactive);
+});
 
-        var marker7  = L.marker([38.246741, 21.738961], { icon: TakenDonation }).bindPopup('TakenDonation');
-        var marker8  = L.marker([38.243944, 21.740098], { icon: TakenDonation }).bindPopup('TakenDonation');
+var toggleMarkersButton3 = document.getElementById('toggleMarkersBtn3');
+toggleMarkersButton3.addEventListener('click', function() {
+    toggleMarkersByType(markersGroup2Active);
+});
 
-        var marker9  = L.marker([38.247719, 21.740227], { icon: nonTakenDonation }).bindPopup('NonTakenDonation');
-        var marker10 = L.marker([38.243287, 21.728798], { icon: nonTakenDonation }).bindPopup('NonTakenDontion');
+var toggleMarkersButton4 = document.getElementById('toggleMarkersBtn4');
+toggleMarkersButton4.addEventListener('click', function() {
+    toggleMarkersByType(markersGroup2Inactive);
+});
 
-        var marker11 = L.marker([38.252117, 21.738038], { icon: TakenRequest }).bindPopup('TakenRequest');
-        var marker12 = L.marker([38.256582, 21.743338], { icon: TakenRequest }).bindPopup('TakenRequest');
+var toggleMarkersButton5 = document.getElementById('toggleMarkersBtn5');
+toggleMarkersButton5.addEventListener('click', function() {
+    toggleMarkersByType(markersGroup3Active);
+})
 
-        var marker13 = L.marker([38.249134, 21.743767], { icon: nonTakenRequest }).bindPopup('nonTakenRequest');
-        var marker14 = L.marker([38.240877, 21.733832], { icon: nonTakenRequest }).bindPopup('nonTakenRequest');
+var toggleMarkersButton6 = document.getElementById('toggleMarkersBtn6');
+toggleMarkersButton6.addEventListener('click', function() {
+    toggleMarkersByType(markersGroup3Inactive);
+})
 
-        markersGroup1.addLayer(marker1).addLayer(marker2);
-        markersGroup1.addTo(map);
-
-        markersGroup2.addLayer(marker3).addLayer(marker4);
-        markersGroup2.addTo(map);
-
-        markersGroup3.addLayer(marker5).addLayer(marker6);
-        markersGroup3.addTo(map);
-
-        markersGroup4.addLayer(marker7).addLayer(marker8);
-        markersGroup4.addTo(map);
-
-        markersGroup5.addLayer(marker9).addLayer(marker10);
-        markersGroup5.addTo(map);
-
-        markersGroup6.addLayer(marker11).addLayer(marker12);
-        markersGroup6.addTo(map);
-
-        markersGroup7.addLayer(marker13).addLayer(marker14);
-        markersGroup7.addTo(map);
-
-        function toggleMarkers1() {
-            if (map.hasLayer(markersGroup1)) {
-                map.removeLayer(markersGroup1);
-            } else {
-                map.addLayer(markersGroup1);
-            }
-        }
-
-        function toggleMarkers2() {
-            if (map.hasLayer(markersGroup2)) {
-                map.removeLayer(markersGroup2);
-            } else {
-                map.addLayer(markersGroup2);
-            }
-        }
-
-        function toggleMarkers3() {
-            if (map.hasLayer(markersGroup3)) {
-                map.removeLayer(markersGroup3);
-            } else {
-                map.addLayer(markersGroup3);
-            }
-        }
-
-        function toggleMarkers4() {
-            if (map.hasLayer(markersGroup4)) {
-                map.removeLayer(markersGroup4);
-            } else {
-                map.addLayer(markersGroup4);
-            }
-        }
-
-        function toggleMarkers5() {
-            if (map.hasLayer(markersGroup5)) {
-                map.removeLayer(markersGroup5);
-            } else {
-                map.addLayer(markersGroup5);
-            }
-        }
-
-        function toggleMarkers6() {
-            if (map.hasLayer(markersGroup6)) {
-                map.removeLayer(markersGroup6);
-            } else {
-                map.addLayer(markersGroup6);
-            }
-        }
-
-        function toggleMarkers7() {
-            if (map.hasLayer(markersGroup7)) {
-                map.removeLayer(markersGroup7);
-            } else {
-                map.addLayer(markersGroup7);
-            }
-        }
-    
-
-    // Event listener for button click to toggle markers
-    var toggleMarkersButton1 = document.getElementById('toggleMarkersBtn1');
-    toggleMarkersButton1.addEventListener('click', toggleMarkers1);
-    
-
-    var toggleMarkersButton2 = document.getElementById('toggleMarkersBtn2');
-    toggleMarkersButton2.addEventListener('click', toggleMarkers2);
-    
-    var toggleMarkersButton3 = document.getElementById('toggleMarkersBtn3');
-    toggleMarkersButton3.addEventListener('click', toggleMarkers3);
-
-    var toggleMarkersButton4 = document.getElementById('toggleMarkersBtn4');
-    toggleMarkersButton4.addEventListener('click', toggleMarkers4);
-
-    var toggleMarkersButton5 = document.getElementById('toggleMarkersBtn5');
-    toggleMarkersButton5.addEventListener('click', toggleMarkers5);
-
-    var toggleMarkersButton6 = document.getElementById('toggleMarkersBtn6');
-    toggleMarkersButton6.addEventListener('click', toggleMarkers6);
-
-    var toggleMarkersButton7 = document.getElementById('toggleMarkersBtn7');
-    toggleMarkersButton7.addEventListener('click', toggleMarkers7);
+// Fetch markers when the page is ready
+fetchMarkers();
 
     </script>
 </body>
-</html>
