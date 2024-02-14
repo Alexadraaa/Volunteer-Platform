d26c2e2b7c183704c64e69afbd416f258cc81ad3
Marianthi Thodi <anthoula_thodi@hotmail.gr>
2024-02-14 23:34:22 +0200

    update databse name into web

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "testing_final";
+$database = "web";
 
 // Create a connection
 $conn = new mysqli($servername, $username, $password, $database);
