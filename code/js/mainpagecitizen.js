be007ecc4a289f5f20475023d8d05ff4ed6e57e9
Marianthi Thodi <anthoula_thodi@hotmail.gr>
2024-02-14 19:42:16 +0200

    Delete all files and folders

--- code/js/mainpagecitizen.js
+++
@@ -1,7 +0,0 @@
- document.getElementById('volunteers-counter').innerText = '250';
-document.getElementById('rescuers-counter').innerText = '120';
-document.getElementById('vehicles-counter').innerText = '30';
-
-function scrollToContactForm() {
-     document.getElementById('main-content').scrollIntoView({ behavior: 'smooth' });
-}
